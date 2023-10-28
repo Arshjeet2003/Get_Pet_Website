4e9a16d847985f93164d2c1ddbf50b56df6c6975
Arshjeet2003 <arshjeetsingh123@gmail.com>
2023-10-28 21:03:32 +0530

    uri updated

--- server/db.js
+++ server/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://arsh_1709:RpzAe2w2SfbbKmMY@practisecluster.kcs8xis.mongodb.net/get_pet";
+var dotenv = require('dotenv')
+
+dotenv.config();
+const mongoURI = process.env.MONGO_URL;
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
